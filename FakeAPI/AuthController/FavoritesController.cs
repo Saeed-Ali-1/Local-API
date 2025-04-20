51fb8fdc2f2fc9d6db1ac9de8b2829114de2e0cc
Saeed-Ali-1 <saidalisaid292@gmail.com>
2025-04-21 00:49:47 +0200

    New Commets

---
+++ FakeAPI/AuthController/FavoritesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FakeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoritesController : ControllerBase
+    {
+        [HttpPost("addToFavorites")]
+        public IActionResult AddToFavorites()
+        {
+            return Ok(new { message = "Movie added to favorites!" });
+        }
+
+        [HttpGet("getFavorites")]
+        public IActionResult GetFavorites()
+        {
+            var favorites = new[]
+            {
+                new {
+                    _id = "6005a12c5d03ca24dcaf3146",
+                    movieName = "Cast Away",
+                    imgUrl = "https://www.pluggedin.com/9.jpg",
+                    userID = "5edd7a356573a6001774adb8",
+                    movieID = "12345"
+                }
+            };
+
+            return Ok(new
+            {
+                message = "success",
+                favorites = favorites
+            });
+        }
+    }
+}
